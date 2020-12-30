d2208b2e62b43561b8eb6d38df7f3f8a74cbee60
SriRitaUtami <sririta0411@gmail.com>
2020-12-30 12:03:12 +0700

    Membuat code Mobil

--- PROJECT PBO/Mobil.java
+++ PROJECT PBO/Mobil.java
@@ -1,51 +1,53 @@
 public class Mobil {
-	Private String merk;
-	Private String jenis;
-	Private String transmisi;
+	
+		private String merk;
+		private String jenis;
+		private String transmisi;
 
-	public String getMerk()
-	{
-		return merk;
-	}
+		public String getMerk()
+		{
+			return merk;
+		}
 
-	public String getTransmisi()
-	{
-		return transmisi;
-	}
+		public String getTransmisi()
+		{
+			return transmisi;
+		}
 
-	public String getJenis()
-	{
-		return jenis;
-	}
+		public String getJenis()
+		{
+			return jenis;
+		}
 
-	public void setJenis(String jenis)
-	{
-		this.jenis = jenis;
-	}
+		public void setJenis(String jenis)
+		{
+			this.jenis = jenis;
+		}
 
-	public void setMerk(String merk)
-	{
-		this.merk = merk;
-	}
+		public void setMerk(String merk)
+		{
+			this.merk = merk;
+		}
 
-	public void setTransmisi(String transmisi)
-	{
-		this.transmisi = transmisi;
-	}
+		public void setTransmisi(String transmisi)
+		{
+			this.transmisi = transmisi;
+		}
 
-	public void tampildata()
-	{
-		System.out.println("Mobil dengan Merk : "+getMerk());
-		System.out.println("Jenis Mobil : "+getJenis());
-		System.out.println("Transmisi Mobil : "+getTransmisi());
-	}
+		public void tampildata()
+		{
+		
+			System.out.println("Mobil dengan Merk : "+getMerk());
+			System.out.println("Jenis Mobil : "+getJenis());
+			System.out.println("Transmisi Mobil : "+getTransmisi());
+		}
+	
 
-	public void inputdata (String m, String j, String t)
-	{
-		setMerk(m);
-		setJenis(j);
-		setTransmisi(t);
+		public void inputdata (String m, String j, String t)
+		{
+			setMerk(m);
+			setJenis(j);
+			setTransmisi(t);
+		}
 
-	}
-
-}
+}		
