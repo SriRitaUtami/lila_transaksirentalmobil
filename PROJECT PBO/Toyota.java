842b1df935366385c2a135b584029a7d51be4873
Rini Muhaini <rinimuhaini28@gmail.com>
2020-12-31 15:45:12 +0700

    Membuat class Toyota

--- PROJECT PBO/Toyota.java
+++ PROJECT PBO/Toyota.java
@@ -2,9 +2,9 @@ public class Toyota extends Mobil{
 	public void tampilkanToyota(){
 		String BahanBakar;
 
-		BahanBakar = 'Pertamax';
+		BahanBakar = "Pertamax";
 		Mobil m = new Mobil();
-		m.inputdata('Innova','CVT','Manual');
+		m.inputdata("Innova","CVT","Manual");
 		m.tampildata();
 
 		System.out.println("jenis bahan bakar : "+BahanBakar)
